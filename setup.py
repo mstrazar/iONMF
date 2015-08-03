5e1479ccf29b260b685273bd17d2d752d5389668
mstrazar <martin.strazar@gmail.com>
2015-08-03 10:37:56 +0200

    Added a new version.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'ionmf',
   packages = ['ionmf', 'ionmf.factorization'],
-  version = '0.3.2',
+  version = '0.3.4',
   description = 'Integrative orthogonal non-negative matrix factorization with examples.',
   author = 'Martin Strazar',
   author_email = 'martin.strazar@gmail.com',
   url = 'https://github.com/mstrazar/iONMF',
-  download_url = 'https://github.com/mstrazar/iONMF/tarball/0.3.2',
+  download_url = 'https://github.com/mstrazar/iONMF/tarball/0.3.4',
   keywords = ['data integration', 'matrix factorization', 'machine learning'],
   classifiers = [],
 )
